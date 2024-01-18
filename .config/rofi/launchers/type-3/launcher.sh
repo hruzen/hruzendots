a51e8ae33084385b37f62eb08f9e766017bc806c
hruzen <shubhamritgogoi@gmail.com>
2024-01-18 17:51:18 +0530

    update

--- .config/rofi/launchers/type-3/launcher.sh
+++ .config/rofi/launchers/type-3/launcher.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-## Rofi   : Launcher (Modi Drun, Run, File Browser, Window)
-#
-## Available Styles
-#
-## style-1     style-2     style-3     style-4     style-5*
-## style-6     style-7*     style-8     style-9     style-10
 
 dir="$HOME/.config/rofi/launchers/type-3"
 theme='style-1'
